eef19e0e5741294db9cde1a493f46eb5ba15fe0d
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-12-01 19:47:11 -0500

    update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Donut',
   tagline: 'sweet tools for building your web-based business',
-  url: 'https://donutpower.club',
+  url: 'https://donut.party',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
