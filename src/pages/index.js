1a3156d22bfe210305d013025727334a3194d22e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-12-01 20:20:14 -0500

    console log

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,6 +67,7 @@ function handleSubmit(e){
   e.preventDefault();
   const form=e.target;
   const data = new FormData(form);
+  console.log(form);
   form.querySelector("input[type=email]").disabled=true;
   form.querySelector("input[type=text]").disabled=true;
   form.querySelector("button").disabled=true;
@@ -85,7 +86,7 @@ function handleSubmit(e){
     form.querySelector("span.submitted").classList.remove("hidden");
     alert("Success! Thank you :)");
   })
-};
+}
 
 
 export default function Home() {
